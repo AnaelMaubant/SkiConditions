d953e59c976163366f400562459b4e8dc83c566d
= <=>
2015-03-30 07:11:08 -0400

    Revert "Modification sur ActionBar"
    This reverts commit 6d14ff84bbf575deda53ff0dceeb4c7b27a921d8.

--- SkiConditions/app/src/main/java/ift2905/skiconditions/MainActivity.java
+++ SkiConditions/app/src/main/java/ift2905/skiconditions/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,24 +24,16 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        super.onOptionsItemSelected(item);
-
-        switch(item.getItemId()){
-            case R.id.pref:
-                Toast.makeText(getBaseContext(), "You selected Preference", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.aide:
-                Toast.makeText(getBaseContext(), "You selected Aide", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.info:
-                Toast.makeText(getBaseContext(), "You selected Info ", Toast.LENGTH_SHORT).show();
-                break;
-
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        return true;
 
+        return super.onOptionsItemSelected(item);
     }
 }
