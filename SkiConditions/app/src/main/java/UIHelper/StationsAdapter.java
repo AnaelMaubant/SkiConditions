bb054d352cffcaeaf7e0ce67115d08b8d576a493
AnaelMaubant <anael.maubant@gmail.com>
2015-04-08 09:55:05 -0400

    Adding station search

--- SkiConditions/app/src/main/java/UIHelper/StationsAdapter.java
+++ SkiConditions/app/src/main/java/UIHelper/StationsAdapter.java
@@ -5,9 +5,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -18,12 +21,14 @@ import ift2905.skiconditions.R;
 /**
  * Created by anael on 4/5/2015.
  */
-public class StationsAdapter extends BaseAdapter{
+public class StationsAdapter extends BaseAdapter implements Filterable{
 
     public StationsAdapter(StationsManager stations)
     {
-        _data = new ArrayList();
+        _data = new ArrayList<Map.Entry<String, Station>>();
         _data.addAll(stations.get_stations().entrySet());
+
+        _stationFilterList = _data;
     }
 
     @Override
@@ -59,7 +64,7 @@ public class StationsAdapter extends BaseAdapter{
 
         Map.Entry<String, Station> station = getItem(position);
 
-        ((TextView) result.findViewById(R.id.temperatureText)).setText(station.getValue().get_temperature());
+        ((TextView) result.findViewById(R.id.temperatureText)).setText(station.getValue().get_temperature() + "°C");
         ((TextView) result.findViewById(R.id.StationNameText)).setText(station.getKey());
         ((TextView) result.findViewById(R.id.trailsText)).setText(station.getValue().get_trails().first + "/" + station.getValue().get_trails().second);
 
@@ -84,5 +89,54 @@ public class StationsAdapter extends BaseAdapter{
 
         return result;
     }
-    private ArrayList _data;
+
+    @Override
+    public Filter getFilter()
+    {
+        if(listViewFilter == null)
+        {
+            listViewFilter = new ValueFilter();
+        }
+        return listViewFilter;
+    }
+
+    private class ValueFilter extends Filter
+    {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint)
+        {
+            FilterResults results = new FilterResults();
+
+            if(constraint!=null && constraint.length()>0)
+            {
+                ArrayList<Map.Entry<String, Station>> filterList = new ArrayList<Map.Entry<String, Station>>();
+                for(int i =0; i<_stationFilterList.size(); i++)
+                {
+                    if(_stationFilterList.get(i).getKey().toUpperCase().contains(constraint.toString().toUpperCase()))
+                    {
+                        filterList.add(_stationFilterList.get(i));
+                    }
+                }
+                results.count = filterList.size();
+                results.values = filterList;
+            }
+            else
+            {
+                results.count = _stationFilterList.size();
+                results.values = _stationFilterList;
+            }
+            return results;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results)
+        {
+            _data = (ArrayList<Map.Entry<String, Station>>)results.values;
+            notifyDataSetChanged();
+        }
+    }
+    private ArrayList<Map.Entry<String, Station>> _data;
+    private ValueFilter listViewFilter;
+    private ArrayList<Map.Entry<String, Station>> _stationFilterList;
 }
--- SkiConditions/app/src/main/java/ift2905/skiconditions/DebugListActivity.java
+++ SkiConditions/app/src/main/java/ift2905/skiconditions/DebugListActivity.java
@@ -3,9 +3,12 @@ package ift2905.skiconditions;
 import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import UIHelper.StationsAdapter;
 
@@ -16,6 +19,34 @@ public class DebugListActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug_list);
-        setListAdapter(new StationsAdapter(((SkiConditionApplication)getApplication()).GetStationManager()));
+        adapter = new StationsAdapter(((SkiConditionApplication)getApplication()).GetStationManager());
+        setListAdapter(adapter);
+
+        editText = (EditText)findViewById(R.id.searchStationText);
+        AddSearchListener();
     }
+
+    public void AddSearchListener()
+    {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                DebugListActivity.this.adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private StationsAdapter adapter;
+    private EditText editText;
 }
