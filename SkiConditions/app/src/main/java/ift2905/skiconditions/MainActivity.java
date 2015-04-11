f038e2ae1241803c00686cba23d1ebcfaa2483f5
= <=>
2015-04-10 22:50:22 -0400

    remplir les composants de MainActivity et StationActivity
    lorsqu’on clique sur le nom de la station affiche a la premiere page,
    il nous dirige vers la page de details et remplir la page avec les
    infos(par intent)

--- SkiConditions/app/src/main/java/ift2905/skiconditions/MainActivity.java
+++ SkiConditions/app/src/main/java/ift2905/skiconditions/MainActivity.java
@@ -4,25 +4,47 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.HashMap;
+
+import SkiConditionApi.Station;
+import SkiConditionApi.StationsManager;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    ImageButton imgButton2;
-    ImageButton imgButton3;
-    ImageButton imgButton4;
+    ImageButton button_search,button_favoris,button_random,button_map;
+    TextView view_name,view_temp,view_pistes,view_updateDate;
+    ImageView tempIcon;
+    String t,n,w,d;
+    Pair<Integer,Integer> p;
+    Station s;
+
+    //station affichiee dans la page d'accueil
+    String stationPrincipal="Centre plein air Mont Kanasuta";
+
+    StationsManager sm;
+    HashMap<String, Station> stations;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new DownloadStations().execute();
+
         setContentView(R.layout.activity_main);
+
+        new DownloadStations().execute();
+
         addButtonListener();
         addTextListener();
     }
@@ -42,7 +64,6 @@ public class MainActivity extends ActionBarActivity {
 
         switch (item.getItemId()) {
             case R.id.pref:
-
                 Toast.makeText(getBaseContext(), "You selected Preference", Toast.LENGTH_SHORT).show();
 
                 return true;
@@ -77,6 +98,22 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
+    public void StartTextActivity() {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, StationActivity.class);
+        Bundle bundle = new Bundle();
+
+        //mettre ca dans le bundle
+        bundle.putString("name", stationPrincipal);
+        bundle.putString("temperature", t);
+        bundle.putString("pistes", p.first + "/" + p.second);
+        bundle.putString("update", d);
+
+        intent.putExtras(bundle);
+
+        startActivity(intent);
+    }
+
     private class DownloadStations extends AsyncTask<String, String, Boolean> {
 
         @Override
@@ -91,6 +128,7 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(MainActivity.this, "Error while getting json", Toast.LENGTH_SHORT).show();
                 new LoadStations().execute();
             } else {
+                Log.d("MAIN", "Json Parsing was successful");
                 Toast.makeText(MainActivity.this, "Json Parsing was successful", Toast.LENGTH_SHORT).show();
                 new SaveStations().execute();
             }
@@ -102,7 +140,8 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected Boolean doInBackground(String... params) {
 
-            boolean isASuccess = ((SkiConditionApplication) getApplication()).GetStationManager().SaveStations(getBaseContext());
+            sm = ((SkiConditionApplication) getApplication()).GetStationManager();
+            boolean isASuccess = sm.SaveStations(getBaseContext());
             return isASuccess;
         }
 
@@ -112,7 +151,12 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(MainActivity.this, "Error while saving stations", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(MainActivity.this, "Stations successfully save in db", Toast.LENGTH_SHORT).show();
+
                 new LoadStations().execute();
+
+                Log.d("MAIN", "Stations successfully save in db");
+                stations = sm.get_stations();
+                set_firstPage();
 
             }
         }
@@ -123,7 +167,8 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected Boolean doInBackground(String... params) {
 
-            boolean isASuccess = ((SkiConditionApplication) getApplication()).GetStationManager().LoadStations(getBaseContext());
+            sm = ((SkiConditionApplication) getApplication()).GetStationManager();
+            boolean isASuccess = sm.LoadStations(getBaseContext());
             return isASuccess;
         }
 
@@ -132,53 +177,89 @@ public class MainActivity extends ActionBarActivity {
             if (!saveIsASuccess) {
                 Toast.makeText(MainActivity.this, "Error while loading stations", Toast.LENGTH_SHORT).show();
             } else {
+                Log.d("MAIN", "Stations successfully load from db");
                 Toast.makeText(MainActivity.this, "Stations successfully load from db", Toast.LENGTH_SHORT).show();
+                stations = sm.get_stations();
+                set_firstPage();
 
             }
         }
     }
 
+    private void findViews(){
+        view_name = (TextView)findViewById(R.id.nomStation);
+        view_temp = (TextView)findViewById(R.id.temp);
+        view_pistes= (TextView)findViewById(R.id.pistes);
+        tempIcon=(ImageView)findViewById(R.id.tempIcon);
+        view_updateDate= (TextView)findViewById(R.id.emis);
+
+    }
+
+    //first page
+    private void showResults(){
+        // on recupere la premiere station
+        s = stations.get(stationPrincipal);
+        t = s.get_temperature();
+        n = s.get_name();
+        p = s.get_trails();
+        w = s.get_weather();
+        d = s.get_lastUpdate();
+
+        // mettre ca dans l interface
+        view_name.setText(s.get_name());
+        view_temp.setText(t);
+        view_pistes.setText(p.first + "/" + p.second);
+        view_updateDate.setText(d);
+    }
+
+    public void set_firstPage(){
+        findViews();
+        showResults();
+    }
 
     public void addButtonListener() {
-        ImageButton imgButton1 = (ImageButton) findViewById(R.id.chercherButton);
-        imgButton1.setOnClickListener(new View.OnClickListener() {
+        button_search = (ImageButton) findViewById(R.id.chercherButton);
+        button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                
                 Toast.makeText(MainActivity.this, "chercher une station!", Toast.LENGTH_SHORT).show();
             }
         });
 
-        imgButton2 = (ImageButton) findViewById(R.id.favorisButton);
-        imgButton2.setOnClickListener(new View.OnClickListener() {
+        button_favoris = (ImageButton) findViewById(R.id.favorisButton);
+        button_favoris.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "montrer tous mes sites preferees!", Toast.LENGTH_SHORT).show();
             }
         });
 
-        imgButton3 = (ImageButton) findViewById(R.id.randomButton);
-        imgButton3.setOnClickListener(new View.OnClickListener() {
+        button_random = (ImageButton) findViewById(R.id.randomButton);
+        button_random.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "suggerer maintenant une station aleatoire!", Toast.LENGTH_SHORT).show();
             }
         });
 
-        imgButton4 = (ImageButton) findViewById(R.id.mapButton);
-        imgButton4.setOnClickListener(new View.OnClickListener() {
+        button_map = (ImageButton) findViewById(R.id.mapButton);
+        button_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "montrer la carte qui indique tous les stations!", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
     public void addTextListener() {
-    TextView btn = (TextView) findViewById(R.id.nomStation);
-    btn.setOnClickListener(new View.OnClickListener() {
+        TextView btn = (TextView) findViewById(R.id.nomStation);
+        btn.setOnClickListener(new View.OnClickListener() {
 
         @Override
         public void onClick (View v){
             Toast.makeText(MainActivity.this, "Afficher maintenant la page de details de cette station!!", Toast.LENGTH_SHORT).show();
+            StartTextActivity();
 
         }
     }
--- SkiConditions/app/src/main/java/ift2905/skiconditions/StationActivity.java
+++ SkiConditions/app/src/main/java/ift2905/skiconditions/StationActivity.java
@@ -1,29 +1,36 @@
 package ift2905.skiconditions;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import SkiConditionApi.Station;
 
 
 public class StationActivity extends Activity {
-    ImageButton imgButton1;
-    ImageButton imgButton2;
-    ImageButton imgButton3;
-    ImageButton imgButton4;
+    ImageButton imgButton1,imgButton2,imgButton3,imgButton4;
+    TextView view_name,view_temp,view_pistes,view_updateDate;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station);
+        findViews();
+        getResults();
         addButtonListener();
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,6 +88,32 @@ public class StationActivity extends Activity {
         });
     }
 
+    public void findViews(){
+
+        view_name = (TextView)findViewById(R.id.nomStation_activity2);
+        view_temp= (TextView)findViewById(R.id.temperature_activity2);
+        view_pistes= (TextView)findViewById(R.id.pistes_activity2);
+        view_updateDate= (TextView)findViewById(R.id.emis_activity2);
+
+    }
+
+    public void getResults(){
+
+        Bundle bundle = this.getIntent().getExtras();
+        String name = bundle.getString("name");
+        String temp = bundle.getString("temperature");
+        String pistes = bundle.getString("pistes");
+        String update = bundle.getString("update");
+
+        // mettre ca dans l interface
+        view_name.setText(name);
+        view_temp.setText(temp);
+        view_pistes.setText(pistes);
+        view_updateDate.setText(update);
+
+    }
 }
+
+
 
 
